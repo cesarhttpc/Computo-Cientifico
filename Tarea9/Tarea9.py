edf186cb032ca552db5aafaf37028a3fac50644a
cesarhttpc <ce_rarin@hotmail.com>
2023-12-04 05:38:50 -0600

    tarea 9

--- Tarea9/Tarea9.py
+++ Tarea9/Tarea9.py
@@ -1,26 +1,9 @@
 # %%
 import numpy as np
 import random
-from scipy.stats import uniform, beta 
+from scipy.stats import uniform, beta ,bernoulli, poisson, hypergeom, gamma
 import matplotlib.pyplot as plt
 from scipy.special import gammaln
-
-
-# def objetivo(p, N, x):
-
-#     m = len(x) 
-#     suma = 0
-#     for i in range(m):
-
-#         # termino = - np.log( np.math.factorial(x[i])) - np.log( np.math.factorial(N - x[i]))
-#         termino = - np.log(float(np.math.factorial(x[i]))) - np.log(float(np.math.factorial(N - x[i])))
-
-        
-#         suma = termino + suma
-
-#     f = m*np.log( np.math.factorial(N)) + suma + np.sum(x) *np.log(p) + (m* N - np.sum(x) ) *np.log(1-p) + 19*np.log(1-p)
-
-#     return f
 
 
 def objetivo(p, N, x):
@@ -40,47 +23,44 @@ def betadist(p):
     return 19*np.log( 1- p )
 
 
-# objetivo(0.5,7,np.array([3,4,5]))
-
-
-
-
-
-
-# %%
-
-
 def MetropolisHastingsHibrido(x,tamañoMuestra = 10000):
 
     # Punto inicial
-    x_0 = np.array([0.5,10])
+    N_max = 1000
+    w = np.arange(max(x)+ 1, N_max)
+
+    p_0 = uniform.rvs(0,1)
+    N_0 = random.choice(w)
 
     Cadena_p = np.zeros(tamañoMuestra)  # p
     Cadena_N = np.zeros(tamañoMuestra)  # N
-    Cadena_p[0] = x_0[0]
-    Cadena_N[0] = x_0[1] 
-
+    Cadena_p[0] = p_0
+    Cadena_N[0] = N_0
 
     for k in range(tamañoMuestra-1):
 
-
-        N_max = 1000
-
-
         # Kernel híbrido
-        # indices = [1,2,3,4,5]
-        indices = [2]
+        indices = [1,2,3,4,5]
         j = random.choice(indices)
 
         # Simulación de propuesta
         if j == 1:
 
+            Cadena_N[k+1] = Cadena_N[k]
+            a = np.sum(x) +1
+            l = len(x)*Cadena_N[k]- np.sum(x) + 20
+            p = gamma.rvs(a, scale = 1/l)
 
-            pass
-            # alpha = CadenaAlpha[k]
+            f_y = objetivo(p, Cadena_N[k], x)
+            f_x = objetivo(Cadena_p[k],Cadena_N[k],x)
+            q_y = gamma.logpdf(p,a,scale = 1/l)
+            q_x = gamma.logpdf(Cadena_p[k], a,scale = 1/l)
 
-            # CadenaAlpha[k+1] = CadenaAlpha[k]
-            # CadenaLambda[k+1] = gamma.rvs(alpha + 20 , 1/(1 +  sum(t**alpha)))
+            if np.exp(f_y - f_x + q_x - q_y) > uniform.rvs(0,1):
+                Cadena_p[k+1] = p
+
+            else:
+                Cadena_p[k+1] = Cadena_p[k]
 
 
         elif j == 2:
@@ -102,54 +82,62 @@ def MetropolisHastingsHibrido(x,tamañoMuestra = 10000):
                 Cadena_N[k+1] = Cadena_N[k]
 
 
+        elif j == 3:
+
+            Cadena_p[k+1] = Cadena_p[k] 
+            N = Cadena_N[k] + hypergeom.rvs(50,9,5)
+
+            f_y = objetivo(Cadena_p[k], N, x)
+            f_x = objetivo(Cadena_p[k],Cadena_N[k],x)
+            q_x = hypergeom.logpmf(Cadena_N[k],50,9,5)
+            q_y = hypergeom.logpmf(N,50,9,5)
+            if np.exp(f_y - f_x + q_x - q_y) > uniform.rvs(0,1):
+                Cadena_N[k+1] = N 
+
+            else:
+                Cadena_N[k+1] = Cadena_N[k]
 
 
+        elif j == 4 : 
+
+            Cadena_p[k+1] = Cadena_p[k] 
+
+            N = Cadena_N[k] + poisson.rvs(1)
+
+            if N <= N_max:
 
 
-        # elif j == 3:
+                f_y = objetivo(Cadena_p[k], N, x)
+                f_x = objetivo(Cadena_p[k],Cadena_N[k],x)
 
-        #     alpha_p = expon.rvs(0) 
-        #     lamda_p = gamma.rvs(alpha_p, scale = 1)
+                if np.exp(f_y - f_x ) > uniform.rvs(0,1):
+                    Cadena_N[k+1] = N 
+
+                else:
+                    Cadena_N[k+1] = Cadena_N[k]
+            else: 
+
+                Cadena_N[k+1] = Cadena_N[k]
 
 
-        #     f_y = LogObjetivo(alpha_p,lamda_p,t)
-        #     f_x = LogObjetivo(CadenaAlpha[k], CadenaLambda[k],t)
-        #     q_x = LogPropuesta3(CadenaAlpha[k],CadenaLambda[k])
-        #     q_y = LogPropuesta3(alpha_p,lamda_p)
+        elif j == 5 :
 
-        #     if np.exp(f_y + q_x - f_x - q_y) > uniform.rvs(0,1):
+            Cadena_p[k+1] = Cadena_p[k]
+            N = Cadena_N[k] + 2*bernoulli.rvs(1/2)-1
 
-        #         CadenaAlpha[k+1] = alpha_p
-        #         CadenaLambda[k+1] = lamda_p
-        #     else:
-                
-        #         CadenaAlpha[k+1] = CadenaAlpha[k]   
-        #         CadenaLambda[k+1] = CadenaLambda[k]
+            f_y = objetivo(Cadena_p[k], N, x)
+            f_x = objetivo(Cadena_p[k],Cadena_N[k],x)
 
-        # else:
+            if np.exp(f_y - f_x ) > uniform.rvs(0,1):
+                Cadena_N[k+1] = N 
 
-        #     CadenaLambda[k+1] = CadenaLambda[k]
-            
-        #     sigma = 0.5
-        #     e = norm.rvs(0,sigma)
-        #     alpha_p = CadenaAlpha[k] + e
-
-        #     f_y = LogObjetivo(alpha_p,CadenaLambda[k+1],t)
-        #     f_x = LogObjetivo(CadenaAlpha[k], CadenaLambda[k],t)
-
-        #     if np.exp(f_y-f_x) > uniform.rvs(0,1):
-
-        #         CadenaAlpha[k+1] = alpha_p
-        #     else:
-                
-        #         CadenaAlpha[k+1] = CadenaAlpha[k]   
-            
-
+            else:
+                Cadena_N[k+1] = Cadena_N[k]
 
     return Cadena_p, Cadena_N
 
 
-
+#Datos dados
 x = np.array([7,7,8,8,9,4,7,5,5,6,9,8,11,7,5,5,7,3,10,3])
 
 Cadena_p, Cadena_N = MetropolisHastingsHibrido(x)
@@ -167,27 +155,11 @@ plt.plot(Cadena_p,Cadena_N)
 
 
 
-# %%
-n = 24
-z = np.arange(n)
-print(z)
-lst = []
-for i in range(100000):
-
-    lst.append(random.choice(z))
-
-plt.hist(lst, bins = n)
-
-
-print(lst)
-
 
 
 # %%
 
-# np.math.factorial(4)
 
-
-x = np.array([1,3,2,5])
-
-np.math.factorial(x)
+# hypergeom.rvs(10,5)
+hypergeom.logpmf(154,10000,2000, 800)
+hypergeom.rvs(10000,2000,800)
